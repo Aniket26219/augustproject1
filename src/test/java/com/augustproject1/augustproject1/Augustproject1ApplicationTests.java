de2a3d5448128d79bf36c3b896caa0ff5d4d93ce
ANIKET <aniket98shahane@gmail.com>
2019-08-14 11:30:17 +0530

    3 pacakage demo

---
+++ src/test/java/com/augustproject1/augustproject1/Augustproject1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.augustproject1.augustproject1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Augustproject1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
